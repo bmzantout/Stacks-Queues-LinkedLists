0f226de4fc4492f788efdcaa717e8d203d2e2f67
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-04-12 21:45:10 +0300

    Update Node_Q1B.java

--- Question 1 (Part B)/Node_Q1B.java
+++ Question 1 (Part B)/Node_Q1B.java
@@ -5,9 +5,6 @@ public class Node_Q1B {
 	//-----------------------------------------------------
 	// Title: Question 1 - Part A
 	// Author: Basme Zantout
-	// ID: 99227947762
-	// Section: 1
-	// Assignment: 1
 	// Description: This is the class used by Question 1 (Part B) which is
 	//              a Node class that creates a Node and has the required
 	//              getter and setter methods for it. It also does the required
